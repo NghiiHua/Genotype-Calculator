09f6e6fcc7f2c680d0c314fbe34d7dd3276f47fd
silentwhales <68089703+silentwhales@users.noreply.github.com>
2022-07-13 11:18:05 -0400

    Update Genotype Calculator.py

--- Genotype Calculator.py
+++ Genotype Calculator.py
@@ -1,22 +1,68 @@
+# input to start
+# for quantity of traits being crossed
 traitsquantity = input(
     "How many traits are you crossing? Ex. 1 is monohybrid and 2 is dihybrid.")
 
+# initializing strings
+# for mother
 mother = 1
 mothergenotype = ""
 
+# create mother genotype
+# repeat until all alleles for traits are determined
 while mother <= int(traitsquantity):
-    if len(str(mothergenotype)) % 2 == 1 or str(mothergenotype) == "":
-        evaluate = input("Is the first allele of the MOTHER trait "
+    # determine between 1st or 2nd allele
+    if len(str(mothergenotype)) % 2 == 0 or str(mothergenotype) == "":
+        # ask for first allele of the trait
+        evaluate = input("Is the FIRST allele of the MOTHER trait "
                          + str(mother) + " rec or dom?")
+        # create the mother genotype
         mothergenotype = mothergenotype + " " + str(evaluate)
-        evaluate = input("Is the second allele of the MOTHER trait "
+        # ask for second allele of the trait
+        evaluate = input("Is the SECOND allele of the MOTHER trait "
                          + str(mother) + " rec or dom?")
+        # create the mother genotype
         mothergenotype = mothergenotype + " " + str(evaluate)
+        # moving to next trait
         mother = int(mother) + 1
-if mother > int(traitsquantity):
-    for element in mothergenotype:
-        if element != "rec" or "dom":
-            mothergenotype = "Restart and print either 'dom' or 'rec'."
-            break
+    else:
+        # ask for second allele of the trait
+        # considering alleles for more than one trait
+        evaluate = input("Is the FIRST allele of the MOTHER trait "
+                         + str(mother) + " rec or dom?")
+        # create the mother genotype
+        mothergenotype = mothergenotype + " " + str(evaluate)
+        # moving to next trait
+        mother = int(mother) + 1
 
-print(mothergenotype)
+# initializing strings
+# for father
+father = 1
+fathergenotype = ""
+
+# create father genotype
+# repeat until all alleles for traits are determined
+while father <= int(traitsquantity):
+    # determine between 1st or 2nd allele
+    if len(str(fathergenotype)) % 2 == 0 or str(fathergenotype) == "":
+        # ask for first allele of the trait
+        evaluate = input("Is the FIRST allele of the FATHER trait "
+                         + str(father) + " rec or dom?")
+        # create the father genotype
+        fathergenotype = fathergenotype + " " + str(evaluate)
+        # ask for second allele of the trait
+        evaluate = input("Is the SECOND allele of the FATHER trait "
+                         + str(mother) + " rec or dom?")
+        # create the father genotype
+        fathergenotype = fathergenotype + " " + str(evaluate)
+        # moving to next trait
+        father = int(father) + 1
+    else:
+        # ask for second allele of the trait
+        # considering alleles for more than one trait
+        evaluate = input("Is the SECOND allele of the FATHER trait "
+                         + str(father) + " rec or dom?")
+        # create the father genotype
+        fathergenotype = fathergenotype + " " + str(evaluate)
+        # moving to next trait
+        father = int(father) + 1
