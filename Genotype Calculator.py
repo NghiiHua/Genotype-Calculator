e1d2cbd11c0e9d50e28fd76173a76ee622accf66
silentwhales <68089703+silentwhales@users.noreply.github.com>
2022-07-13 02:59:59 -0400

    Update Genotype Calculator.py

--- Genotype Calculator.py
+++ Genotype Calculator.py
@@ -4,7 +4,7 @@ traitsquantity = input(
 mother = 1
 mothergenotype = ""
 
-if mother <= int(traitsquantity):
+while mother <= int(traitsquantity):
     if len(str(mothergenotype)) % 2 == 1 or str(mothergenotype) == "":
         evaluate = input("Is the first allele of the MOTHER trait "
                          + str(mother) + " rec or dom?")
@@ -13,9 +13,10 @@ if mother <= int(traitsquantity):
                          + str(mother) + " rec or dom?")
         mothergenotype = mothergenotype + " " + str(evaluate)
         mother = int(mother) + 1
-
-for element in range(len(mothergenotype)):
-    if element != "rec" or "dom":
-        mothergenotype = "Restart and print either 'dom' or 'rec'."
+if mother > int(traitsquantity):
+    for element in mothergenotype:
+        if element != "rec" or "dom":
+            mothergenotype = "Restart and print either 'dom' or 'rec'."
+            break
 
 print(mothergenotype)
