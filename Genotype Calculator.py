e73f49222c3b1ff9cd879cb513e317745807d879
silentwhales <68089703+silentwhales@users.noreply.github.com>
2022-07-19 09:46:37 -0400

    Update Genotype Calculator.py

--- Genotype Calculator.py
+++ Genotype Calculator.py
@@ -13,9 +13,12 @@ mothergenotype = ""
 while mother <= int(traitsquantity):
     # determine between 1st or 2nd allele
     if len(str(mothergenotype)) % 2 == 0 or str(mothergenotype) == "":
-        # ask for first allele of the trait
         evaluate = input("Is the FIRST allele of the MOTHER trait "
                          + str(mother) + " rec or dom?")
+        if evaluate == "dominant":
+            mothergenotype == mothergenotype + "1"
+        elif evaluate == "recessive":
+            mothergenotype ==
         # create the mother genotype
         mothergenotype = mothergenotype + " " + str(evaluate)
         # ask for second allele of the trait
@@ -34,35 +37,3 @@ while mother <= int(traitsquantity):
         mothergenotype = mothergenotype + " " + str(evaluate)
         # moving to next trait
         mother = int(mother) + 1
-
-# initializing strings
-# for father
-father = 1
-fathergenotype = ""
-
-# create father genotype
-# repeat until all alleles for traits are determined
-while father <= int(traitsquantity):
-    # determine between 1st or 2nd allele
-    if len(str(fathergenotype)) % 2 == 0 or str(fathergenotype) == "":
-        # ask for first allele of the trait
-        evaluate = input("Is the FIRST allele of the FATHER trait "
-                         + str(father) + " rec or dom?")
-        # create the father genotype
-        fathergenotype = fathergenotype + " " + str(evaluate)
-        # ask for second allele of the trait
-        evaluate = input("Is the SECOND allele of the FATHER trait "
-                         + str(mother) + " rec or dom?")
-        # create the father genotype
-        fathergenotype = fathergenotype + " " + str(evaluate)
-        # moving to next trait
-        father = int(father) + 1
-    else:
-        # ask for second allele of the trait
-        # considering alleles for more than one trait
-        evaluate = input("Is the SECOND allele of the FATHER trait "
-                         + str(father) + " rec or dom?")
-        # create the father genotype
-        fathergenotype = fathergenotype + " " + str(evaluate)
-        # moving to next trait
-        father = int(father) + 1
